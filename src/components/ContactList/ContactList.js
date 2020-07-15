42d2bfcdf7bb3b494b74378330d8a6e96993e16d
PopeyeIT <igorserov28@gmail.com>
2020-07-15 17:13:31 +0300

    ad hw

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ContactList = ({ contacts, deleteItem }) => {
+  return (
+    <ul>
+      {contacts.map(({ name, number, id }) => (
+        <li key={id}>
+          <p>
+            {name}: <span>{number}</span>
+          </p>
+          <button id={id} onClick={deleteItem} type="button">
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
